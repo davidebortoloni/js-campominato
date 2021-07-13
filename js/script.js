0aba6c2ad07988f96ce46047816fae7e710fcf79
davidebortoloni <davidebortoloni@gmail.com>
2021-07-13 17:03:29 +0200

    Aggiunta difficoltà

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
-let mines = [];
-let attempts = [];
 numberSection = document.getElementById("number-section");
 numberField = document.getElementById("number");
-message = document.getElementById("message");
+difficultySection = document.getElementById("difficulty-section");
+difficultyButton = document.getElementById("start");
 submitButton = document.getElementById("submit");
 resetButton = document.getElementById("reset");
+message = document.getElementById("message");
+let total;
+let mines = [];
+let attempts = [];
 
-while (mines.length < 16) {
-    const number = randomNumber();
-    if (!mines.includes(number)) {
-        mines.push(number);
+difficultyButton.addEventListener("click", function () {
+    switch (displayRadioValue("difficulty")) {
+        case "easy":
+            total = 100;
+            break;
+        case "medium":
+            total = 80;
+            break;
+        case "hard":
+            total = 50;
+            break;
     }
-}
-console.table(mines);
-
+    mines = [];
+    attempts = [];
+    while (mines.length < 16) {
+        const number = randomNumber();
+        if (!mines.includes(number)) {
+            mines.push(number);
+        }
+    }
+    difficultySection.classList.add("d-none");
+    difficultyButton.classList.add("d-none");
+    numberSection.classList.remove("d-none");
+    submitButton.classList.remove("d-none");
+    numberField.value = "";
+})
 submitButton.addEventListener("click", function () {
     const userNumber = parseInt(numberField.value);
-    if (isValid(userNumber)) {
+    if (isValid(userNumber, total)) {
         if (mines.includes(userNumber)) {
             message.innerHTML = "Hai perso! Punteggio: " + attempts.length * 10;
             numberSection.classList.add("d-none");
@@ -28,30 +49,25 @@ submitButton.addEventListener("click", function () {
             } else {
                 message.innerHTML = "Mina schivata";
                 attempts.push(userNumber);
+                if (attempts.length === (total) - (mines.length)) {
+                    message.innerHTML = "Grande, punteggio massimo! <br>" + attempts.length * 10 + " punti";
+                    numberSection.classList.add("d-none");
+                    submitButton.classList.add("d-none");
+                    resetButton.classList.remove("d-none");
+                }
             }
         }
     }
-    console.table(attempts);
 })
 resetButton.addEventListener("click", function () {
-    mines = [];
-    attempts = [];
-    while (mines.length < 16) {
-        const number = randomNumber();
-        if (!mines.includes(number)) {
-            mines.push(number);
-        }
-    }
     message.innerHTML = "";
-    numberField.value = ""
-    numberSection.classList.remove("d-none");
-    submitButton.classList.remove("d-none");
+    difficultySection.classList.remove("d-none");
+    difficultyButton.classList.remove("d-none");
     resetButton.classList.add("d-none");
-    console.table(mines);
 })
 
 function isValid(num) {
-    if (num <= 0 || num > 100) {
+    if (num <= 0 || num > total) {
         alert("Inserire un numero compreso tra 1 e 100");
         return false;
     }
@@ -59,4 +75,12 @@ function isValid(num) {
 }
 function randomNumber() {
     return Math.floor(Math.random() * 100) + 1;
+}
+function displayRadioValue(name) {
+    var ele = document.getElementsByName(name);
+    for (i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            return ele[i].value;
+        }
+    }
 }
